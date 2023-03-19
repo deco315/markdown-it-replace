e872d164882a86d3e4e358a8b2adf06f5bfb5dc9
deco313 <deco313@proton.me>
2023-03-19 21:56:55 +0400

    Initial commit

---
+++ src/renderer.ts
@@ -0,0 +1,13 @@
+import type { RenderRule } from 'markdown-it/lib/renderer'
+import type Token from 'markdown-it/lib/token'
+
+export default function renderer(): RenderRule {
+  // returns html
+  return function (tokens: Token[], idx: number) {
+    const token = tokens[idx]
+    const word = token.content
+    const applyRule = token.meta.rule
+
+    return applyRule(word)
+  }
+}
