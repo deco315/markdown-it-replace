4e3fef8ebd8f1e6c31c2d1647fe8388a21b98d69
accplan <accplan@rambler.ru>
2023-03-24 19:27:41 +0400

    refactor

---
+++ src/tokenizer/WordSearcher.ts
@@ -0,0 +1,126 @@
+import type { Rule, Word } from '../types'
+
+export class WordSearcher {
+  #searchString = ''
+  #searchRule?: Rule
+  #rules: Rule[] = []
+  #results: Word[] = []
+
+  setSearchString(str: string): this {
+    this.#searchString = str
+    return this
+  }
+
+  addRules(rules: Rule[]): this {
+    this.#rules = [...this.#rules, ...rules]
+    return this
+  }
+
+  #checkParams() {
+    if (!this.#searchRule) {
+      throw new Error('You forgot to set a search rule. Please do this by calling the method ".setRule()"')
+    }
+  }
+
+  findRegexp(): this {
+    this.#checkParams()
+    const pattern = this.#searchRule!.pattern
+
+    if (pattern instanceof RegExp) {
+      const res = this.#searchString.match(pattern)
+      if (!res) {
+        return this
+      }
+
+      const foundWord = res[1] || res[0] || ''
+
+      this.#results.push({
+        content: foundWord,
+        position: {
+          start: res.index || 0,
+          end: (res.index || 0) + foundWord.length
+        },
+        transform: this.#searchRule!.transform
+      })
+    }
+    return this
+  }
+
+  findArray(): this {
+    this.#checkParams()
+    const pattern = this.#searchRule!.pattern
+
+    if (Array.isArray(pattern)) {
+      for (const word of pattern) {
+        const found = findWordString(word, this.#searchString)
+        
+        if (found) {
+          this.#results.push({
+            ...found,
+            transform: this.#searchRule!.transform
+          })
+        }
+      }
+    }
+    return this
+  }
+
+  findString(): this {
+    this.#checkParams()
+    const pattern = this.#searchRule!.pattern
+
+    if (typeof pattern === 'string') {
+      const word = findWordString(pattern, this.#searchString)
+      if (word) {
+        this.#results.push({
+          ...word,
+          transform: this.#searchRule!.transform
+        })
+      }
+    }
+    return this
+  }
+
+  findAll(): Word[] {
+    this.#rules.forEach(rule => {
+      this.#searchRule = rule
+      this.findString().findArray().findRegexp()
+    })
+    return this.#results
+  }
+
+  findOne(sortFunction: (word1: Word, word2: Word) => boolean) {
+    const results = this.findAll()
+    let result = results.pop()
+
+    if (!result || results.length === 0) {
+      return result || null
+    }
+
+    for (const rc of results) {
+      if (sortFunction(rc, result)) {
+        result = rc
+      }
+    }
+
+    return result
+  }
+}
+
+export function wordSearcher() {
+  return new WordSearcher()
+}
+
+function findWordString(search: string, str: string): Omit<Word, 'transform'> | null {
+  if (str.indexOf(search) === -1) {
+    return null
+  }
+
+  return {
+    content: search,
+    position: {
+      start: str.indexOf(search),
+      end: str.indexOf(search) + search.length
+    }
+  }
+}
--- src/tokenizer/findWord.ts
+++ src/tokenizer/findWord.ts
@@ -1,83 +1,14 @@
 import type { Rule, Word } from '../types'
 
+import { wordSearcher } from './WordSearcher'
+
 /**
  * Find the first occurrence of the pattern.
  * Return its content with corresponding start and end positions in the state
  */
 export function findWord(rules: Rule[], str: string): Word | null {
-  let resultCandidates: Word[] = []
-
-  for (const rule of rules) {
-    const pattern = rule.pattern
-
-    if (pattern instanceof RegExp) {
-      const res = str.match(pattern)
-      if (!res) {
-        continue
-      }
-
-      const foundWord = res[1] || res[0] || ''
-
-      resultCandidates.push({
-        content: foundWord,
-        position: {
-          start: res.index || 0,
-          end: (res.index || 0) + foundWord.length
-        },
-        transform: rule.transform
-      })
-    }
-
-    if (Array.isArray(pattern)) {
-      for (const word of pattern) {
-        const found = findWordString(word, str)
-        
-        if (found) {
-          resultCandidates.push({
-            ...found,
-            transform: rule.transform
-          })
-        }
-      }
-      continue
-    }
-
-    if (typeof pattern === 'string') {
-      const word = findWordString(pattern, str)
-      if (word) {
-        resultCandidates.push({
-          ...word,
-          transform: rule.transform
-        })
-      }
-    }
-  }
-
-  let result = resultCandidates.pop()
-
-  if (!result || resultCandidates.length === 0) {
-    return result || null
-  }
-
-  for (const rc of resultCandidates) {
-    if (rc.position.start < result.position.start) {
-      result = rc
-    }
-  }
-
-  return result
-}
-
-function findWordString(search: string, str: string): Omit<Word, 'transform'> | null {
-  if (str.indexOf(search) === -1) {
-    return null
-  }
-
-  return {
-    content: search,
-    position: {
-      start: str.indexOf(search),
-      end: str.indexOf(search) + search.length
-    }
-  }
+  return wordSearcher()
+      .setSearchString(str)
+      .addRules(rules)
+      .findOne((word1, word2) => word1.position.start < word2.position.start)
 }
